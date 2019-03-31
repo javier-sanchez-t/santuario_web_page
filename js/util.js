ea7eac8f1f9092558b8dba48f41fa6c848de6684
Javier Sanchez <jefe10jav@gmail.com>
2019-03-31 17:21:05 -0600

    JST - Send email and make calls

--- js/util.js
+++ js/util.js
@@ -22,3 +22,72 @@ setInputFilter(document.getElementById("cantidad"), function (value) {
 setInputFilter(document.getElementById("noReservacion"), function (value) {
     return /^-?\d*$/.test(value);
 });
+
+function clearModalFacturacion() {
+    document.getElementById("noReservacion").value = '';
+    document.getElementById("fechaReservacion").value = '';
+    document.getElementById("cantidad").value = '';
+    document.getElementById("rfc").value = '';
+    document.getElementById("extranjero").checked = false;
+}
+
+function sendFacturasForm() {
+    var noReservacion = document.getElementById("noReservacion").value;
+    var fechaViaje = document.getElementById("fechaReservacion").value;
+    var cantidad = document.getElementById("cantidad").value;
+    var RFC = document.getElementById("rfc").value;
+    var extranjero = document.getElementById("extranjero").checked;
+
+    var bodyEmail = "<strong>No. Reservación:</strong> " + noReservacion + "<br/>" +
+        "<strong>Fecha de viaje:</strong> " + fechaViaje + "<br/>" +
+        "<strong>Cantidad:</strong> $" + cantidad;
+
+    if (RFC != null && RFC != undefined && RFC != "") {
+        bodyEmail += "<br/><strong>RFC:</strong> " + RFC;
+    }
+
+    if (extranjero) {
+        bodyEmail += "<br/><strong>Extranjero: </strong> Sí";
+    } else {
+        bodyEmail += "<br/><strong>Extranjero: </strong> No";
+    }
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "jefe10jav@gmail.com",
+        Password: "javi10%st",
+        To: 'jefe10jav@gmail.com',
+        From: "jefe10jav@gmail.com",
+        Subject: "Solicitud de facturación",
+        Body: bodyEmail
+    }).then(
+        message => {
+            $('#facturaModal').modal('hide')
+
+            if (message == 'OK') {
+                alert("Sus datos han sido enviados, en breve atenderemos su solicitud. \n\n ¡Gracias!");
+            } else {
+                alert("Ocurrió un error con su solicitud, inténtelo más tarde.");
+            }
+        }
+    );
+}
+
+
+/*function sendMailFacturacion() {
+    var noReservacion = document.getElementById("noReservacion").value;
+    var fechaViaje = document.getElementById("fechaReservacion").value;
+    var cantidad = document.getElementById("cantidad").value;
+    var RFC = document.getElementById("rfc").value;
+
+    var bodyEmail = "No. Reservación: " + noReservacion + "<br/>" +
+        "Fecha de viaje: " + fechaViaje + "\n" +
+        "Cantidad: " + cantidad + "\n";
+
+    var link = "mailto:jefe10jav@gmail.com"
+        + "?subject=Solicitud de facturación"
+        + "&body=" + bodyEmail;
+    ;
+
+    window.location.href = link;
+}*/
